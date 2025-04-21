804e9411b79289cbe78a07c73ae3b50c09cea07a
Dev-Abishek-Rajagopal-Official <official@abishekrajagopal.com>
2025-04-20 22:26:56 -0400

    Update TimelineSection.jsx

--- medical_front/src/components/TimelineSection.jsx
+++ medical_front/src/components/TimelineSection.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import TimelineChart from './TimelineChart.jsx';
 
+/**
+ * Renders a section containing a timeline chart.
+ *
+ * @component
+ * @param {Object} props - Component props.
+ * @param {Array<Object>} props.arrayData - Array of data points for the timeline chart.
+ * @returns {JSX.Element} A section containing the TimelineChart component if data is provided.
+ */
 const TimelineSection = ({ arrayData }) => (
   <div>
     {arrayData && <TimelineChart arrayData={arrayData} />}
