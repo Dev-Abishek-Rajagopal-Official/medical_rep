fc4d4f10e35628c8db1a88b6abdbad62e8684dac
Dev-Abishek-Rajagopal-Official <official@abishekrajagopal.com>
2025-04-20 22:25:27 -0400

    Update StreamedContent.jsx

--- medical_front/src/components/StreamedContent.jsx
+++ medical_front/src/components/StreamedContent.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
+/**
+ * Component for displaying streamed content in Markdown format.
+ *
+ * @component
+ * @param {Object} props - Component props
+ * @param {string} props.streamedContent - Markdown-formatted string to display
+ * @returns {JSX.Element} Rendered StreamedContent component
+ */
 const StreamedContent = ({ streamedContent }) => (
   <div>
     <ReactMarkdown>{streamedContent}</ReactMarkdown>
