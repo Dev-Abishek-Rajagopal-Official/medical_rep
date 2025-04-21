974b4b5b37524b3b0eca129162edfd2999155924
Dev-Abishek-Rajagopal-Official <official@abishekrajagopal.com>
2025-04-20 22:22:51 -0400

    Update PreviousConversations.jsx

--- medical_front/src/components/PreviousConversations.jsx
+++ medical_front/src/components/PreviousConversations.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const PreviousConversations = ({ conversations, isLoadingConversations, handlePreviousQueryClick }) => (
+/**
+ * Component to display a list of previous conversations as clickable buttons.
+ *
+ * @component
+ * @param {Object} props - Component props
+ * @param {Array} props.conversations - List of previous conversation objects
+ * @param {boolean} props.isLoadingConversations - Loading state for conversations
+ * @param {Function} props.handlePreviousQueryClick - Callback when a previous conversation is clicked
+ * @returns {JSX.Element} Rendered previous conversations section
+ */
+const PreviousConversations = ({
+  conversations,
+  isLoadingConversations,
+  handlePreviousQueryClick
+}) => (
   <div>
     <hr />
     <h6>Previous Conversations</h6>
